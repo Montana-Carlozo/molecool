7feb8d9bd7006b7ebef6429220250db04b871585
Montana Carlozo <mcarlozo@nd.edu>
2022-02-01 13:45:01 -0500

    Added doctring

--- molecool/functions.py
+++ molecool/functions.py
@@ -7,6 +7,22 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
 def canvas(with_attribution=True):
+    """
+    Placeholder function to show example of docstring (NumPy format)
+    
+    Replace this function and doc string for your own project
+    
+    Parameters
+    ----------
+    with_attribution : bool, Optional, default: True
+        Set whether or not to display who quote is from
+    
+    Returns
+    -------
+    quote : str
+        Compiled quote including string and optional attribution
+    """
+
     quote = "The code is but a canvas to our imagination."
     if with_attribution:
         quote += "\n\t - Adapted by H.D.T"
