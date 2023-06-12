da5b4de0872853873608cdd5572ac929d141acd5
Montana Carlozo <mcarlozo@nd.edu>
2023-06-12 14:38:19 -0400

    Added modules and submodules by splitting functions.py

--- molecool/__init__.py
+++ molecool/__init__.py
@@ -3,6 +3,13 @@ This was created for the Python Best Practices Workshop"""
 
 # Add imports here
 from .functions import canvas
+# from .measure import calculate_angle, calculate_distance
+from molecool.measure import calculate_angle, calculate_distance #Same as above line
+from molecool.atom_data import atomic_weights, atom_colors
+from molecool.visualization import draw_molecule 
+
+from molecool import io
+from molecool.molecules import bond_histogram, build_bond_list
 
 
 from ._version import __version__
